06b128584c0def6a89526edb7903608a17f57a36
James Dawson <jamesdawsonWD@gmail.com>
2019-09-26 00:16:06 +0100

    added a new vision for the palers surroundings

--- Assets/RotateItem.cs
+++ Assets/RotateItem.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FollowCamera : MonoBehaviour
+public class RotateItem : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class FollowCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, 0, 2 * Time.deltaTime); //rotates 50 degrees per second around z axis
     }
 }
